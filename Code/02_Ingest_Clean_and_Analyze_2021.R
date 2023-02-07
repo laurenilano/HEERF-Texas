fb42d833b3e12a478464ba17bc8e49deeb4b3d25
Lauren Ilano <laurenilano@gmail.com>
2023-02-07 14:28:38 -0800

    ingest raw HEERF 2021 data

--- Code/02_Ingest_Clean_and_Analyze_2021.R
+++ Code/02_Ingest_Clean_and_Analyze_2021.R
@@ -1,3 +1,29 @@
 setwd("~/Dropbox/EdTrust/HEERF_Git")
 library(tidyverse)
 library(readxl)
+
+# ------------------------------------------------------------------------------
+# Step 1: Download data and DED from https://covid-relief-data.ed.gov/
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Step 2: Review DED to familiarize yourself with the type of data available
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Step 3: Ingest 2020 HEERF Raw Data
+# ------------------------------------------------------------------------------
+
+HEERF_2021 <- read_excel("Raw Data/2021 ESF Data Files/heer-2021-public-2023-02.xlsx",
+                    sheet= "Prime")
+
+Institutional_Information_2021 <- read_excel("Raw Data/2021 ESF Data Files/heer-2021-public-2023-02.xlsx",
+                                        sheet= "Enriched")
+MSI <- read_csv("Raw Data/MSI.csv", 
+                col_types = cols(HBCU = col_double(), 
+                                 PBI = col_double(), 
+                                 ANNHI = col_double(), 
+                                 TRIBAL = col_double(), 
+                                 AANAPII = col_double(), 
+                                 HSI = col_double(), 
+                                 NANTI = col_double())) 
