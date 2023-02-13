eda8ddb71b6d1ac12929081f244d76762316b68f
Lauren Ilano <laurenilano@gmail.com>
2023-02-13 12:14:04 -0800

    select relevant 2021 vars

--- Code/00_Get_MSI_Data.R
+++ Code/00_Get_MSI_Data.R
@@ -1,14 +1,14 @@
 # Download MSI Flags from Scorecard
-setwd("~/Dropbox/EdTrust/HEERF")
+setwd("~/Dropbox/EdTrust")
 
 library(tidyverse)
 
 # ingest raw scorecard data
-scorecard <- read_csv("Raw Data/Scorecard-Latest-Institution.csv")
+scorecard <- read_csv("Scorecard-Latest-Institution.csv")
 
 # select only relevant variables for MSI flags
 MSI <- scorecard %>%
   select(INSTNM, OPEID, UNITID, HBCU, PBI, ANNHI, TRIBAL, AANAPII, HSI, NANTI)
 
 # write as MSI.csv
-write_csv(MSI, "Raw Data/MSI.csv")
+write_csv(MSI, "HEERF_Git/Raw Data/MSI.csv")
