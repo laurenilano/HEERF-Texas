dc4676a14b2cf59f8edad54347ddff0838842b0d
Lauren Ilano <laurenilano@gmail.com>
2023-02-25 14:45:32 -0800

    calculate withdrawal rate

--- Code/02_Ingest_Clean_and_Analyze_2021.R
+++ Code/02_Ingest_Clean_and_Analyze_2021.R
@@ -174,15 +174,29 @@ Texas_2021_HEERF <- HEERF_2021 %>%
          enroll2020GW,
          enroll2019Ug,
          enroll2019UgW,
+         enroll2019UgC,
+         enroll2019UgSe,
          enroll2019G,
          enroll2019GW) %>%
   filter(state == "TEXAS") %>%
   left_join(Institutional_Information_2021, by = c("dunsNumber"="duns")) %>%
   left_join(MSI, by = c("unitid" = "UNITID")) %>% # unitid is from Inst Information 2021
-  # remove 3 institutions with no match on duns (261 -> 258)
+  # *****!!!! remove 3 institutions with no match on duns (261 -> 258)
   filter(!is.na(instnm)) %>%
   # exclude institutions with data issues -- none, use allEnrolledStudents, not ipeds numIheStudents (not 12 month)
-  mutate(distributed_to_more_than_enrolled = ifelse(allEnrolledStudents * 1.10 < allStudentsHeerRecipients, 1, 0))
+  mutate(distributed_to_more_than_enrolled = ifelse(allEnrolledStudents * 1.10 < allStudentsHeerRecipients, 1, 0)) %>%
+  select(unitid, opeid, instnm, stabbr, iheType, iheControl, pctNative, pctAsian, pctBlack, pctHispanic, # Inst Information 2021
+         pctIslander, pctWhite, pctMultRaces, pctFullTime, pctPartTime, pctPell, hbcu, pbi, annhi, # Inst Information 2021
+         tribal, aanapii, hsi, nanti,  ## Inst Information 2021
+        contains("awardedAmounts"), awards, ends_with("PellEnrolled"), ends_with("Enrolled"),
+        ends_with("HeerRecipients"), starts_with("a1"), starts_with("a2"), starts_with("a3"),
+        starts_with("a4"), starts_with("minimum"), starts_with("maximum"), ends_with("GrantAmountTotal"),
+        ends_with("AverageHeerAmount"), ends_with("EligibleTitleIV"), ends_with("EnrolledStudentCount"),
+        ends_with("ReceivedAidGrant"), ends_with("TotalAmountOfGrants"), ends_with("AverageHeerfAward"),
+        starts_with("enroll2021"), starts_with("enroll2020"), starts_with("enroll2019")) %>%
+  # in awardedAmounts, force NA to 0
+  mutate(across(starts_with("awardedAmounts"), ~replace(., is.na(.), 0)),
+         across(ends_with("HeerRecipients"), ~replace(., is.na(.), 0)))
 
 # Additional Cleaning Steps:
 # Force NA to 0
@@ -196,6 +210,21 @@ Texas_2021_HEERF <- HEERF_2021 %>%
 ## Spending by MSI Type
 
 ## Institution Level
+
+
+### OUTCOME: Still Enrolled
+
+# Check out variables
+Outcome <- Texas_2021_HEERF %>%
+  select(instnm, starts_with("enroll2019"))
+
+# Withdrawal Rate (Only ~ 50 institutions have this info...)
+Withdraw_Rate <- Texas_2021_HEERF %>%
+  select(instnm, starts_with("enroll")) %>%
+  mutate(UG_Withdraw_Rate_2019 = (enroll2019UgW/enroll2019Ug) * 100,
+         UG_Withdraw_Rate_2020 = (enroll2020UgW/enroll2020Ug) * 100,
+         UG_Withdraw_Rate_2021 = (enroll2021UgW/enroll2021Ug) * 100) %>%
+  select(instnm, contains("Withdraw_Rate"))
 
 
 
